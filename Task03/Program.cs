46842119594d26bd00192cb0db47b1cdfea63448
Rudik <lexa.arsenal.rudik@gmail.com>
2023-01-15 16:05:58 +0300

    Исправили решение задачи Task03

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -6,35 +6,32 @@
 Console.WriteLine("Введите число от 1 до 7");
 int number = Convert.ToInt32(Console.ReadLine());
 
-if (number == 1 )
+if (number == 1)
 {
     Console.WriteLine("Это понедельник");
 }
-if (number == 2 )
+else if (number == 2)
 {
     Console.WriteLine("Это вторник ");
 }
-if (number == 3 )
+ else if (number == 3)
 {
     Console.WriteLine("Это среда");
 }
-if (number == 4 )
+ else if (number == 4)
 {
     Console.WriteLine("Это четверг");
 }
-if (number == 5 )
+else if (number == 5)
 {
     Console.WriteLine("Это пятница");
 }
-if (number == 6 )
+else if (number == 6)
 {
     Console.WriteLine("Это суббота");
 }
-if (number == 7 )
+else if (number == 7)
 {
     Console.WriteLine("Это воскресенье");
 }
- else 
- {
-    Console.WriteLine("Число не подходит, введите число от 1 до 7 "); 
- }
+else Console.WriteLine("Число не подходит, введите число от 1 до 7 ");
