32ac1b4339c82a6ca0dd64af1240a5a1cf55f49e
Rudik <lexa.arsenal.rudik@gmail.com>
2023-01-15 16:43:55 +0300

    Сделали исправления в задаче  Task05

--- Task05/Program.cs
+++ Task05/Program.cs
@@ -7,8 +7,15 @@
 Console.WriteLine("Введите число");
 int num = Convert.ToInt32(Console.ReadLine());
 int count = -num;
+if (num > 0)
+{
 while (count <= num)
 {
     Console.Write(count + " ");
     count ++;
+}
+}
+else 
+{
+    Console.WriteLine("Введено не натуральное число, пожалуйста, введите натуральное число !");
 }
