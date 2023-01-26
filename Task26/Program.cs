c96437fc9047b16723020049010407f075fa3961
Rudik <lexa.arsenal.rudik@gmail.com>
2023-01-26 15:01:42 +0300

    Добавили задачи Task24,26,28,30

---
+++ Task26/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 26: Напишите программу, которая 
+// 1.принимаетн на вход число и 
+// 2,выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+int countNumber = CountNumber(number);
+Console.WriteLine($"{number} -> {countNumber}");
+
+int CountNumber(int num)
+{
+    if (num < 0) num = num * -1;
+    int count = 0;
+    while (num > 0)
+    {
+        num = num / 10;
+        count++;
+    }
+    return count;
+}
---
+++ Task28/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 28: 
+// Напишите программу, которая
+// 1.принимает на вход число N и 
+// 2.выдаёт произведение чисел от 1 до N.
+
+// 4 -> 24
+// 5 -> 120
+
+int Factorial(int num)
+{
+    int productnum = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        checked   //проверка ошибки
+        {
+            productnum = productnum * i;
+        }
+    }   
+        return productnum;
+    }
+   
+
+    Console.Write("Введите целое число: ");
+    int number;
+    if (int.TryParse(Console.ReadLine(), out number))
+    {
+        if (number > 0)
+            Console.WriteLine($"Факториал числа {number} = {Factorial(number)}");
+        else
+            Console.WriteLine("Введено некорректное значение. Введите натуральное число.");
+    }
+    else
+        Console.WriteLine("Введено некорректное значение. Введите целое число");
